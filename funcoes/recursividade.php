265a94a9d61f16de43632dd09525dfe2235a1138
Evandro Siqueira <evandrosiquer@hotmail.com>
2020-04-24 19:52:48 -0300

    Funções - Parte 2

---
+++ funcoes/recursividade.php
@@ -0,0 +1,29 @@
+<div class="titulo">Recursividade</div>
+
+<?php
+function somaUmAte($numero) {
+    $soma = 0;
+    for($i = 1;$i <= $numero; $i++){
+      $soma +=$i;
+    }
+    return $soma;
+}
+
+echo somaUmAte(5) . '<br>';
+echo somaUmAte(10) . '<br>';
+
+function somaRecursivaUmAte($numero) {
+    //Condição de parada
+    if($numero ===1){
+        return 1;
+    }
+    return $numero + somaRecursivaUmAte($numero -1);
+}
+
+echo somaRecursivaUmAte(150) .'<br>';
+
+function somaRecursivaEconomica($numero) {
+    return $numero === 1 ? 1 : $numero + somaRecursivaEconomica($numero -1);
+}
+
+echo somaRecursivaEconomica(15) . '<br>';
--- index.php
+++ index.php
@@ -317,6 +317,21 @@
                                 Funções Anônimas
                              </a>
                            </li>
+                           <li>
+                             <a href="exercicio.php?dir=funcoes&file=closure_callable">
+                                Closure e Callable
+                             </a>
+                           </li>
+                           <li>
+                             <a href="exercicio.php?dir=funcoes&file=recursividade">
+                                Recursividade
+                             </a>
+                           </li>
+                           <li>
+                             <a href="exercicio.php?dir=funcoes&file=desafio_recursividade">
+                                Desafio Recursividade
+                             </a>
+                           </li>
                         </ul>   
                      </div>                         
             </nav>
